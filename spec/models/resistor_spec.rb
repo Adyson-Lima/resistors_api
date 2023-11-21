4c3cf8d586f57b555a7ae4501275f3d85032ebcd
Adyson-Lima <adyson_lima@yahoo.com.br>
2023-11-21 16:00:16 -0300

    2° commit - tdd model: res_type consegue ser preenchido? RED

---
+++ spec/models/resistor_spec.rb
@@ -0,0 +1,16 @@
+require "rails_helper"
+
+RSpec.describe Resistor, type: :model do
+
+  before{@resistor = Resistor.new}
+
+  describe "Teste de preenchimento do model Resistor" do
+
+    it "res_type consegue ser preenchido?" do
+      @resistor.res_type = ""
+      expect(@resistor.res_type).to eq("smd")
+    end
+
+  end
+
+end
